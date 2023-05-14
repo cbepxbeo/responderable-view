f494c9dd439639d585b3181ccdb58d0bb16d045d
cbepxbeo <cbepxbeo@mail.ru>
2023-05-14 19:56:26 +0300

    basic

---
+++ Tests/ResponderableViewTests/Shared/ResponderableViewTests.swift
@@ -0,0 +1,13 @@
+/*
+ 
+ Project: ResponderableView
+ File: ResponderableViewTests.swift
+ Created by: Egor Boyko
+ Date: 14.05.2023
+ 
+*/
+
+import XCTest
+@testable import ResponderableView
+
+final class ResponderableViewTests: XCTestCase {}
